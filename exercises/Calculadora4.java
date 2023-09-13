e2dc055af8e0eff807c02e675d68d91e5d7ee2ec
Davi_Nascimento <123577817+TheGoldenBoy23@users.noreply.github.com>
2023-09-13 20:26:49 -0300

    .

--- exercises/Calculadora4.java
+++ exercises/Calculadora4.java
@@ -15,46 +15,75 @@ public class App {
             opcao = scanner.next().charAt(0);
 
             switch(opcao) {
+                // SOMAR
                 case '+':
-                    System.out.print("Digite o primeiro valor: ");
-                    valor1 = scanner.nextFloat();
-                    System.out.print("Digite o segundo valor: ");
-                    valor2 = scanner.nextFloat();
-                    System.out.printf("Resultado: %.2f\n", (valor1 + valor2));
+                try{ 
+                        System.out.print("Digite o primeiro valor: ");
+                        valor1 = scanner.nextFloat();
+                        System.out.print("Digite o segundo valor: ");
+                        valor2 = scanner.nextFloat();
+                        System.out.printf("Resultado: %.2f\n", (valor1 + valor2));
+                        break;
+                }
+                catch(Exception e) {
+                    System.out.println("\nValor invalido. Tente novamente\n");
                     break;
+                }
                     
+                // SUBTRAIR
                 case '-':
+                try{ 
                     System.out.print("Digite o primeiro valor: ");
                     valor1 = scanner.nextFloat();
                     System.out.print("Digite o segundo valor: ");
                     valor2 = scanner.nextFloat();
                     System.out.printf("Resultado: %.2f\n", (valor1 - valor2));
                     break;
+                }
+                catch(Exception e) {
+                    System.out.println("\nValor invalido. Tente novamente\n");
+                    break;
+                }
 
+                // MULTIPLICAR
                 case '*':
+                try{ 
                     System.out.print("Digite o primeiro valor: ");
                     valor1 = scanner.nextFloat();
                     System.out.print("Digite o segundo valor: ");
                     valor2 = scanner.nextFloat();
                     System.out.printf("Resultado: %.2f\n", (valor1 * valor2));
                     break;
+                }
+                catch(Exception e) {
+                    System.out.println("\nValor invalido. Tente novamente\n");
+                    break;
+                }
 
+                // DIVIDIR
                 case '/':
+                try{ 
                     System.out.print("Digite o primeiro valor: ");
                     valor1 = scanner.nextFloat();
                     System.out.print("Digite o segundo valor: ");
                     valor2 = scanner.nextFloat();
+                    // cant divide by 0
+                    if(valor2 == 0) {
+                        System.out.println("Invalido");
+                        break;
+                    }
+                    else {
+                        System.out.printf("Resultado: %.2f\n", (valor1 / valor2));
+                        break;
 
-                        // cant divide by 0
-                        if(valor2 == 0) {
-                            System.out.println("Invalido");
-                            break;
-                        }
-                        else {
-                            System.out.printf("Resultado: %.2f\n", (valor1 / valor2));
-                            break;
+                    }
+                }
+                catch(Exception e) {
+                    System.out.println("\nValor invalido. Tente novamente\n");
+                    break;
+                }
 
-                        }
+                // SAIR
                 case 's':
                         break;
 
@@ -64,5 +93,6 @@ public class App {
                 }
 
         }
+        scanner.close();
     }
 }   
